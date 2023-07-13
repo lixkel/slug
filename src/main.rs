33e716252484137de62a6cc97982b5d95a230042
Marek Mojík <marekmojik@protonmail.com>
2023-07-13 23:54:58 +0200

    changing dir structure

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,13 @@ use cli::parse_args;
 
 fn main() {
     let args = parse_args();
+
+    match args.file {
+        Some(string) => {
+            println!("The value is: {}", string);
+        },
+        None => {
+            println!("The value is None.");
+        }
+    }
 }
