022511c58e57ac9a4f808473809f805bd77aba60
Marek Mojík <marekmojik@protonmail.com>
2023-12-11 20:04:54 +0100

    fix statistics bugs

--- src/statistics.rs
+++ src/statistics.rs
@@ -1,16 +1,20 @@
+use crate::parser::PerfData;
+
 use std::collections::HashMap;
 use std::error::Error;
 
 type StatFunction = fn(&PerfData) -> Result<f32, Box<dyn Error>>;
 
 struct StatCalculator {
+    name: &'static str,
     function: StatFunction,
     required_keys: Vec<&'static str>,
 }
 
-pub fn calculate_stats(perf_data: &PerfData) -> Result<HashMap<String, f32>, Box<dyn Error>> {
+pub fn calculate_stats(perf_data: &mut PerfData) -> Result<HashMap<String, f32>, Box<dyn Error>> {
     let stat_functions = vec![
         StatCalculator {
+            name: "average",
             function: calculate_average,
             required_keys: vec!["min", "max"],
         },
@@ -21,7 +25,7 @@ pub fn calculate_stats(perf_data: &PerfData) -> Result<HashMap<String, f32>, Box
     for calculator in stat_functions {
         if calculator.required_keys.iter().all(|&key| perf_data.contains_key(key)) {
             if let Ok(result) = (calculator.function)(perf_data) {
-                stats.insert(format!("{:?}", calculator.function), result);
+                stats.insert(calculator.name.to_string(), result);
             }
         }
     }
