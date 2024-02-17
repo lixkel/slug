94de65a791d3afa14da2551fe45cf518e3183000
Marek Mojík <marekmojik@protonmail.com>
2024-02-17 23:25:20 +0100

    add get_latest function to retrieve data from database

--- src/dbm.rs
+++ src/dbm.rs
@@ -1,7 +1,23 @@
 use crate::parser::PerfData;
 
-use rusqlite::{self, Connection};
+use rusqlite::{self, Connection, Rows};
+use std::collections::HashMap;
 use std::error::Error;
+
+
+
+impl PerfData {
+    fn from_row(rows: Rows, name: String) -> Result<PerfData, Box<dyn Error>> {
+        let data = PerfData {
+            name: name,
+            map: HashMap::new(),
+        };
+
+        
+
+        Ok(data)
+    }
+}
 
 fn get_conn() -> Result<Connection, Box<dyn Error>> {
     Ok(Connection::open("slug.db")?)
@@ -36,4 +52,19 @@ fn insert_conn(conn: &mut Connection, data: &PerfData) -> Result<(), Box<dyn Err
     conn.execute(&insert_stmt, values.as_slice())?;
 
     Ok(())
+}
+
+fn get_latest(conn: &mut Connection, data: &PerfData) -> Result<PerfData, Box<dyn Error>> {
+    let keys = data.map.keys()
+        .map(|key| key.as_str())
+        .collect::<Vec<&str>>();
+
+    let keys_str = keys.join(", ");
+
+    let read_stmt = format!("SELECT {} FROM {} ORDER BY id DESC LIMIT 1;", keys_str, data.name);
+
+    let mut stmt = conn.prepare(&read_stmt)?;
+    let mut row = stmt.query([])?;
+
+    PerfData::from_row(row, data.name.clone())
 }
