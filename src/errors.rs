db6fca2bd01718c9ccc392d937730661c25c234a
Marek Mojík <marekmojik@protonmail.com>
2025-11-17 02:11:36 +0100

    Implement unified SlugError type
    Introduce unified error type for all application errors
    This error type also wraps library specific error types

---
+++ src/errors.rs
@@ -0,0 +1,69 @@
+use std::{error, fmt};
+
+#[derive(Debug)]
+pub enum SlugError {
+    Git(git2::Error),
+    Io(std::io::Error),
+    Csv(csv::Error),
+    Cli(String),
+    Parsing(String),
+}
+
+impl fmt::Display for SlugError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            SlugError::Git(err) => write!(f, "Git error: {}", err),
+            SlugError::Io(err) => write!(f, "IO error: {}", err),
+            SlugError::Csv(err) => write!(f, "CSV error: {}", err),
+            SlugError::Cli(msg) => write!(f, "CLI error: {}", msg),
+            SlugError::Parsing(msg) => write!(f, "Parsing error: {}", msg),
+        }
+    }
+}
+
+impl error::Error for SlugError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match self {
+            SlugError::Git(err) => Some(err),
+            SlugError::Io(err) => Some(err),
+            SlugError::Csv(err) => Some(err),
+            _ => None,
+        }
+    }
+}
+
+impl From<git2::Error> for SlugError {
+    fn from(err: git2::Error) -> Self {
+        SlugError::Git(err)
+    }
+}
+
+impl From<std::io::Error> for SlugError {
+    fn from(err: std::io::Error) -> Self {
+        SlugError::Io(err)
+    }
+}
+
+impl From<csv::Error> for SlugError {
+    fn from(err: csv::Error) -> Self {
+        SlugError::Csv(err)
+    }
+}
+
+impl From<getopts::Fail> for SlugError {
+    fn from(err: getopts::Fail) -> Self {
+        SlugError::Cli(err.to_string())
+    }
+}
+
+impl From<regex::Error> for SlugError {
+    fn from(err: regex::Error) -> Self {
+        SlugError::Parsing(err.to_string())
+    }
+}
+
+impl From<std::num::ParseFloatError> for SlugError {
+    fn from(err: std::num::ParseFloatError) -> Self {
+        SlugError::Parsing(err.to_string())
+    }
+}
